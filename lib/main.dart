2215ff9d49178f418b71b4f1390f4c9f8c55314d
dan7sc <su7cheng@gmail.com>
2021-07-15 12:17:46 -0300

    Add Post class to models and add CustomDio class to shared

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,8 @@
-import 'dart:convert';
-
-import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
-
-class Post {
-  int? userId;
-  int? id;
-  String? title;
-  String? body;
-
-  Post({this.userId, this.id, this.title, this.body});
-
-  Post.fromJson(Map<String, dynamic> json) {
-    userId = json['userId'];
-    id = json['id'];
-    title = json['title'];
-    body = json['body'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['uerId'] = this.userId;
-    data['id'] = this.id;
-    data['title'] = this.title;
-    data['body'] = this.body;
-    return data;
-  }
-}
+import 'models/post.dart';
+import 'shared/custom_dio/custom_dio.dart';
 
 void getPosts() async {
-  final baseURL = "http://jsonplaceholder.typicode.com";
-  final client = Dio(BaseOptions(baseUrl: baseURL));
+  final client = CustomDio();
   final response = await client.get("/posts");
   print(response.statusCode);
   if (response.statusCode == 200) {
