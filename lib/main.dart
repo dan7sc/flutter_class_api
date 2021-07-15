6b471b73540aead2ff40991d3248ac605f8f3445
dan7sc <su7cheng@gmail.com>
2021-07-15 11:25:33 -0300

    Show requested data on flutter ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Post {
@@ -27,20 +28,73 @@ class Post {
   }
 }
 
-void getPosts() async {
-  final baseURL = "http://jsonplaceholder.typicode.com";
-  var url = Uri.parse(
-    '$baseURL/posts',
-  );
-  final response = await http.get(url);
-  print(response.statusCode);
-  if (response.statusCode == 200) {
-    final json = jsonDecode(response.body) as List;
-    final posts = json.map((e) => Post.fromJson(e)).toList();
-    print(posts);
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
   }
 }
 
-void main() {
-  getPosts();
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Post> posts = List.empty();
+
+  Future<List<Post>> getPosts() async {
+    final baseURL = "http://jsonplaceholder.typicode.com";
+    var url = Uri.parse(
+      '$baseURL/posts',
+    );
+    final response = await http.get(url);
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as List;
+      posts = json.map((e) => Post.fromJson(e)).toList();
+      // print(posts);
+      return posts;
+    }
+    return [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Text(posts[index].id.toString()),
+              title: Text(posts[index].title ?? ""),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await getPosts();
+          setState(() {});
+        },
+        child: Icon(
+          Icons.download,
+          semanticLabel: "Get Posts",
+        ),
+      ),
+    );
+  }
 }
