bca0c1130d0b8f2127b07ff2b828abce13c496ca
dan7sc <su7cheng@gmail.com>
2021-07-15 08:52:03 -0300

    Print response status code

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ void getPosts() async {
   final baseURL = "http://jsonplaceholder.typicode.com";
   var url = Uri.parse('$baseURL/users');
   final response = await http.get(url);
+  print(response.statusCode);
+
   final json = jsonDecode(response.body);
   print(json[0]['name']);
 }
