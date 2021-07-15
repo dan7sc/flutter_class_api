917a852d788741a7f92c803dfb84ba64257308ed
dan7sc <su7cheng@gmail.com>
2021-07-15 09:01:26 -0300

    Make put request that get status code 200

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,13 @@ import 'package:http/http.dart' as http;
 void getPosts() async {
   final baseURL = "http://jsonplaceholder.typicode.com";
   var url = Uri.parse(
-    '$baseURL/posts'
+    '$baseURL/posts/1'
   );
-  final response = await http.post(url, body: {"name": "Daniel"});
+  final response = await http.put(url, body: {"name": "Daniel"});
   print(response.statusCode);
   if(response.statusCode == 200) {
     final json = jsonDecode(response.body);
-    print(json[0]['name']);
+    print(json);
   }
 }
 
