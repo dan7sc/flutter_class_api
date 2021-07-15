b2f3a8c17af696f08a14672d4061578e6eaed0a1
dan7sc <su7cheng@gmail.com>
2021-07-15 13:44:34 -0300

    Add HomeStatus enum

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,18 @@ import 'package:class_api/modules/home/home_controller.dart';
 
 void main() async {
   final controller = HomeController();
+  print(controller.status);
   await controller.getPosts();
-  print('posts: ${controller.posts}');
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
+  print(controller.status);
 }
--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -1,11 +1,15 @@
 import 'package:class_api/modules/home/home_repository.dart';
 import 'package:class_api/shared/models/post.dart';
 
+enum HomeStatus { empty, loading, success, error }
+
 class HomeController {
   final repository = HomeRepository();
   var posts = <Post>[];
+  var status = HomeStatus.empty;
 
   Future<void> getPosts() async {
+    status = HomeStatus.loading;
     final response = await repository.getPosts();
     posts = response;
   }
