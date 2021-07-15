24614081e41c0552e13d4c5bcc8f65c59fc3efa1
dan7sc <su7cheng@gmail.com>
2021-07-15 08:46:19 -0300

    Get name of the first user

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 void getPosts() async {
   final baseURL = "http://jsonplaceholder.typicode.com";
   var url = Uri.parse('$baseURL/users');
   final response = await http.get(url);
-  print(response.body);
+  final json = jsonDecode(response.body);
+  print(json[0]['name']);
 }
 
 void main() {
