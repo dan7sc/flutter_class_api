4758075b337af2db3f0432170db388afae525890
dan7sc <su7cheng@gmail.com>
2021-07-15 08:40:05 -0300

    Get body response

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 void getPosts() async {
   var url = Uri.parse('http://jsonplaceholder.typicode.com/posts');
   final response = await http.get(url);
-  print(response);
+  print(response.body);
 }
 
 void main() {
