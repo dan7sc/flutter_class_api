74adfdaa4ca85578187b1ea6e11e95dfb32bd2ce
dan7sc <su7cheng@gmail.com>
2021-07-15 09:12:33 -0300

    Create Post class using 'JSON to Dart' site

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,42 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+class Post {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  Post.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['uerId'] = this.userId;
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['body'] = this.body;
+    return data;
+  }
+}
+
 void getPosts() async {
   final baseURL = "http://jsonplaceholder.typicode.com";
   var url = Uri.parse(
-    '$baseURL/posts/1'
+    '$baseURL/posts',
   );
-  final response = await http.delete(url, body: {"name": "Daniel"});
+  final response = await http.get(url);
   print(response.statusCode);
-  if(response.statusCode == 200) {
-    final json = jsonDecode(response.body);
-    print(json);
+  if (response.statusCode == 200) {
+    final json = jsonDecode(response.body) as List;
+    final posts = json.map((e) => Post.fromJson(e)).toList();
+    print(posts);
   }
 }
 
